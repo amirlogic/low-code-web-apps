b71ad3c58a7ed837d19522e8cb2ab9157406c203
Amir <amir.hac@outlook.com>
2023-08-14 11:30:56 +0200

    content file

--- cytology/http.js
+++ cytology/http.js
@@ -13,27 +13,7 @@ const SERVER_PORT = 9000
 
 let screen = 'leucocytes';
 
-
-const imglink = { 'monocyte':'https://www.hematocell.fr/images/diaporama/diapositives/49/9.jpg', 
-                  'eosinophile':'https://www.hematocell.fr/images/diaporama/diapositives/49/18.jpg',
-                  'erythrocyte':'https://www.hematocell.fr/images/Images-GR/5gropti.JPG' }
-
-const payload = {
-
-        'leucocytes':[
-            
-            { key:'monocyte', title:"Monocyte", src:imglink['monocyte'], caption:"source: hematocell.fr", body:"This is a monocyte" },
-
-            { key:'monocytebis', title:"Monocyte", src:imglink['monocyte'], caption:"source: hematocell.fr", body:"This is a another monocyte" },
-
-            { key:'eosinophile', title:"Eosinophile", src:imglink['eosinophile'], caption:"source: hematocell.fr", body:"This is an eosinophile" },
-        ],
-
-        'other':[
-
-            { key:'erythrocytes', title:"Erythrocytes", src:imglink['erythrocyte'], caption:"source: hematocell.fr", body:"Erythrocytes (Red blood cells)" }
-        ]
-}
+const { payload } = require('./content')
 
 function pageBody(navtext="NestedLogic",toptext="No title",htw){
 
